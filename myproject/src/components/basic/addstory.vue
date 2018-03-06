b7fb6987df8d3149380e209190312e2f829a351f
TheCrows <qyh751652792@qq.com>
2018-03-06 22:43:20 +0800

    增加添加信息组件

---
+++ myproject/src/components/basic/addstory.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="add-story">
+      <el-row>
+        <el-col :span="18" :offset="1">
+          <div class="grid-content">
+            <el-input v-model="input" placeholder="请输入标题" size="medium"></el-input>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="9" :offset="1">
+          <div class="grid-content">
+            <el-date-picker v-model="value1" type="date" placeholder="选择日期" size="medium"></el-date-picker>
+          </div>
+        </el-col>
+        <el-col :span="9">
+          <div class="grid-content">
+            <el-input v-model="input" placeholder="请输入地点" size="medium"></el-input>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="18" :offset="1">
+          <div class="grid-content">
+            <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 4}" placeholder="请输入内容" v-model="textarea3"></el-input>
+          </div>
+        </el-col>
+        <el-col :span="2" :offset="1">
+          <div class="grid-content">
+              <el-button>提交</el-button>
+          </div>
+        </el-col>
+      </el-row>
+      
+
+
+
+
+  </div>
+</template>
+<script>
+  export default {
+    name: 'addStory',
+    data() {
+      return {
+        value1: '',
+      }
+    }
+  }
+
+</script>
+
+<style lang="scss">
+  .add-story {
+
+    background: #efefef;
+    border-radius: 5px;
+    overflow: auto;
+    padding-bottom: 15px;
+    .el-row{
+        margin-top: 15px;
+    }
+  }
+
+</style>
--- myproject/src/components/basic/homePage.vue
+++ myproject/src/components/basic/homePage.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="home-page">
-      <div style='width:780px; height:3000px'></div>
+      <div class="home-main">
+          <add-story></add-story>
+      </div>
       <user-side></user-side>
       
   </div>
 </template>
 <script>
 import userSide from './userSide'
+import addStory from './addstory'
 export default {
   name:'homePage',
   data(){
@@ -14,10 +17,15 @@ export default {
 
       }
   },
-  components:{userSide}
+  components:{userSide,addStory}
 }
 </script>
 <style lang="scss">
+.home-main{
+    width: 755px;
+    margin-right: 25px;
+    
+}
     .home-page{
         width: 100%;
         display: flex;
