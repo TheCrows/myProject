2354c2bc85748603899937f630bc28a940274bfd
TheCrows <qyh751652792@qq.com>
2018-03-07 22:28:05 +0800

    “完善添加部分

--- myproject/src/components/basic/addstory.vue
+++ myproject/src/components/basic/addstory.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="add-story">
+<div>
+  <div class="add-btn" @click="showAddStory">+</div>
+  <div class="add-story" v-show="ifshow">
       <el-row>
         <el-col :span="18" :offset="1">
           <div class="grid-content">
@@ -19,6 +21,25 @@
           </div>
         </el-col>
       </el-row>
+
+      <el-row>
+        <el-col :span="18" :offset="1">
+          <div class="grid-content">
+        <el-upload
+            action="https://jsonplaceholder.typicode.com/posts/"
+            list-type="picture-card"
+            :on-preview="handlePictureCardPreview"
+            :on-remove="handleRemove"
+            >
+            <i class="el-icon-plus"></i>
+          </el-upload>
+          <el-dialog :visible.sync="dialogVisible">
+            <img width="100%" :src="dialogImageUrl" alt="">
+          </el-dialog>
+          </div>
+        </el-col>
+        </el-row>
+
       <el-row>
         <el-col :span="18" :offset="1">
           <div class="grid-content">
@@ -31,11 +52,7 @@
           </div>
         </el-col>
       </el-row>
-      
-
-
-
-
+  </div>
   </div>
 </template>
 <script>
@@ -44,6 +61,23 @@
     data() {
       return {
         value1: '',
+        input:'',
+        textarea3:'',
+        ifshow:false,
+        dialogImageUrl: '',
+        dialogVisible: false
+      }
+    },
+    methods: {
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handlePictureCardPreview(file) {
+        this.dialogImageUrl = file.url;
+        this.dialogVisible = true;
+      },
+      showAddStory(){
+        this.ifshow=!this.ifshow
       }
     }
   }
@@ -51,6 +85,16 @@
 </script>
 
 <style lang="scss">
+  .add-btn{
+    width: 725px;
+    height: 40px;
+    background: silver;
+     border-radius: 5px;
+     font-weight: 900;
+     font-size: 30px;
+     padding-left: 30px;
+     cursor: pointer;
+  }
   .add-story {
 
     background: #efefef;
@@ -61,5 +105,5 @@
         margin-top: 15px;
     }
   }
-
+ 
 </style>
